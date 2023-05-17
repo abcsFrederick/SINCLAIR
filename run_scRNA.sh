e6498bfae800c51c3e229f1d2f0d4e6e2c9480a7
slsevilla <slsevilla@gmail.com>
2023-05-17 10:39:21 -0400

    change profiles for local run

--- run_scRNA.sh
+++ run_scRNA.sh
@@ -4,21 +4,21 @@ resume=$3
 
 module load nextflow
 
+echo "--Workflow started: $datatype"
 
-if [[ "$resume" == "Y" ]]; then
-    echo "--Workflow started: $datatype"
-
+if [[ $resume == "Y" ]]; then
+    echo "----Resuming pipeline"
     nextflow run main.nf -resume \
     -entry $datatype \
-    -profile docker \
+    -profile biowulf \
     --input assets/input_manifest.csv \
     --outdir /data/sevillas2/scRNA_test \
     --species $species \
 
-elif [[ "$resume" == "N" ]]; then
+elif [[ $resume == "N" ]]; then
     nextflow run main.nf \
     -entry $datatype \
-    -profile docker \
+    -profile biowulf \
     --input assets/input_manifest.csv \
     --outdir /data/sevillas2/scRNA_test \
     --species $species
