8ba71a23ee6b749c3a931c6864a9cc998e5a3ff2
slsevilla <slsevilla@gmail.com>
2023-05-30 09:25:10 -0400

    cleand and comment batch correction scripts

--- bin/scRNA_functions.R
+++ bin/scRNA_functions.R
@@ -1,5 +1,7 @@
-###Run Seurat Clustering 
-seuratClustering = function(so){
+##################################################################
+# Seurat Pre-processing
+##################################################################
+SEURAT_CLUSTERING = function(so){
   
   so$Sample = tail(strsplit(h5,"/")[[1]],1)
   
@@ -19,12 +21,84 @@ seuratClustering = function(so){
   return(so)
 }
 
-doublets <-function(dfso){
+CONVERT_TO_HUMAN_GENELIST  <- function(gns){
+  egs <- mapIds(org.Hs.eg.db, gns, "ENTREZID","SYMBOL")
+  mapped <- select(Orthology.eg.db, egs, "Mus.musculus","Homo.sapiens")
+  mapped$MUS <- mapIds(org.Mm.eg.db, as.character(mapped$Mus.musculus), "SYMBOL", "ENTREZID")
+  return(as.character(unlist(mapped$MUS )))
+}
+
+MAIN_PROCESS_SO<-function(so_in, ref){
+  # so_in=so_filt
+  
+  # assign genes depending on ref input
+  if(ref=="hg38" || ref == "hg19"){
+    print("--proccesing human data")
+    s.genes <- cc.genes$s.genes
+    g2m.genes <- cc.genes$g2m.genes
+  } else if(ref=="mm10"){
+    print("--proccesing mouse data")
+    s.genes <- CONVERT_TO_HUMAN_GENELIST(cc.genes$s.genes)
+    g2m.genes <- CONVERT_TO_HUMAN_GENELIST(cc.genes$g2m.genes)
+  }
+  
+  # process
+  so_1 = SCTransform(so_in)
+  so_2 = NormalizeData(so_1,
+                       normalization.method = "LogNormalize", 
+                       scale.factor = 10000,
+                       assay = "RNA")
+  so_3 = ScaleData(so_2, assay = "RNA")
+  so_4 = CellCycleScoring(so_3,
+                          s.features = s.genes, 
+                          g2m.features = g2m.genes, 
+                          set.ident = TRUE)
+  so_out = SEURAT_CLUSTERING(so_4)
+  return(so_out)
+}
+
+##################################################################
+#
+##################################################################
+RUN_SINGLEr = function(obj,refFile,fineORmain){
+  obj = DietSeurat(obj, graphs = "umap")
+  sce = as.SingleCellExperiment(obj,assay = "SCT")
+  ref = refFile
+  s = SingleR(test = sce, ref = ref,labels = ref[[fineORmain]])
+  return(s$pruned.labels)
+}
+
+MAIN_LABEL_SO<-function(so_in, ref){
+  if(ref == "hg38" || ref == "hg19"){
+    so_in$HPCA_main <- RUN_SINGLEr(so_in,celldex::HumanPrimaryCellAtlasData(),"label.main")
+    so_in$HPCA <-  RUN_SINGLEr(so_in,celldex::HumanPrimaryCellAtlasData(),"label.fine")
+    so_in$BP_encode_main <-  RUN_SINGLEr(so_in,celldex::BlueprintEncodeData(),"label.main")
+    so_in$BP_encode <-  RUN_SINGLEr(so_in,celldex::BlueprintEncodeData(),"label.fine")
+    so_in$monaco_main <-  RUN_SINGLEr(so_in,celldex::MonacoImmuneData(),"label.main")
+    so_in$monaco <-     RUN_SINGLEr(so_in,celldex::MonacoImmuneData(),"label.fine")
+    so_in$immu_cell_exp_main <-  RUN_SINGLEr(so_in,celldex::DatabaseImmuneCellExpressionData(),
+                                            "label.main")
+    so_in$immu_cell_exp <- RUN_SINGLEr(so_in,celldex::DatabaseImmuneCellExpressionData(),
+                                      "label.fine")
+    so_in$annot = so_in$HPCA_main
+  } else if(ref == "mm10"){
+    so_in$immgen_main <-  RUN_SINGLEr(so_in,celldex::ImmGenData(),"label.main")
+    so_in$immgen <- RUN_SINGLEr(so_in,celldex::ImmGenData(),"label.fine")
+    so_in$mouseRNAseq_main <-  RUN_SINGLEr(so_in,celldex::MouseRNAseqData(),"label.main")
+    so_in$mouseRNAseq <- RUN_SINGLEr(so_in,celldex::MouseRNAseqData(),"label.fine")
+    so_in$annot = so_in$immgen_main
+  }
+  return(so_in)
+}
+
+##################################################################
+#
+##################################################################
+PROCESS_DOUBLETS <-function(dfso){
   dfso <- SCTransform(dfso)
   dfso <- RunPCA(dfso, pc.genes = dfso@var.genes, pcs.print = 0,verbose = F,npcs =10)
   npcs = 10
   dfso <- RunUMAP(dfso, verbose=TRUE,dims = 1:npcs)
-  
   
   sweep.res.list_kidney <- paramSweep_v3(dfso,PCs = 1:10, sct = T)
   sweep.stats_kidney <- summarizeSweep(sweep.res.list_kidney, GT = FALSE)
@@ -50,72 +124,73 @@ doublets <-function(dfso){
   return(dfso)
 }
 
-convertHumanGeneList  <- function(gns){
-  egs <- mapIds(org.Hs.eg.db, gns, "ENTREZID","SYMBOL")
-  mapped <- select(Orthology.eg.db, egs, "Mus.musculus","Homo.sapiens")
-  mapped$MUS <- mapIds(org.Mm.eg.db, as.character(mapped$Mus.musculus), "SYMBOL", "ENTREZID")
-  return(as.character(unlist(mapped$MUS )))
+##################################################################
+#
+##################################################################
+RUN_SINGLEr_seurat = function(obj,refFile,fineORmain){
+  # find av expressions
+  avg = AverageExpression(obj,assays = "SCT")
+  avg = as.data.frame(avg)
+  
+  s = SingleR(test = as.matrix(avg),ref = refFile,labels = refFile[[fineORmain]])
+  
+  clustAnnot = s$labels
+  names(clustAnnot) = colnames(avg)
+  names(clustAnnot) = gsub("SCT.","",names(clustAnnot))
+  
+  obj$clustAnnot = clustAnnot[match(obj$seurat_clusters,names(clustAnnot))]
+  return(obj$clustAnnot)
 }
 
-runSingleR = function(obj,refFile,fineORmain){
-  obj = DietSeurat(obj, graphs = "umap")
-  sce = as.SingleCellExperiment(obj,assay = "SCT")
-  ref = refFile
-  s = SingleR(test = sce, ref = ref,labels = ref[[fineORmain]])
-  return(s$pruned.labels)
-}
-
-PROCESS_SO<-function(so_in){
-  # assign genes depending on ref input
-  if(ref=="hg38" || ref == "hg19"){
-    print("--proccesing human data")
-    s.genes <- cc.genes$s.genes
-    g2m.genes <- cc.genes$g2m.genes
-  } else if(ref=="mm10"){
-    print("--proccesing mouse data")
-    s.genes <- convertHumanGeneList(cc.genes$s.genes)
-    g2m.genes <- convertHumanGeneList(cc.genes$g2m.genes)
+RUN_CORRECTION_SEURAT = function(obj,npcs,ref,resolution){
+  # what is this catching? why another scale?
+  if(ncol(obj@assays$integrated@scale.data)!=ncol(obj@assays$integrated@data)){
+    obj = ScaleData(obj)
   }
   
-  # process
-  so_1 = SCTransform(so_in)
-  so_2 = NormalizeData(so_1,
-                       normalization.method = "LogNormalize", 
-                       scale.factor = 10000,
-                       assay = "RNA")
-  so_3 = ScaleData(so_2,assay = "RNA")
-  so_4 = CellCycleScoring(so_3,
-                          s.features = s.genes, 
-                          g2m.features = g2m.genes, 
-                          set.ident = TRUE)
-  so_out = seuratClustering(so_4)
-  return(so_out)
-}
-
-LABEL_SO<-function(so_in){
-  if(ref == "hg38" || ref == "hg19"){
-    so_in$HPCA_main <- runSingleR(so_in,celldex::HumanPrimaryCellAtlasData(),"label.main")
-    so_in$HPCA <-  runSingleR(so_in,celldex::HumanPrimaryCellAtlasData(),"label.fine")
-    so_in$BP_encode_main <-  runSingleR(so_in,celldex::BlueprintEncodeData(),"label.main")
-    so_in$BP_encode <-  runSingleR(so_in,celldex::BlueprintEncodeData(),"label.fine")
-    so_in$monaco_main <-  runSingleR(so_in,celldex::MonacoImmuneData(),"label.main")
-    so_in$monaco <-     runSingleR(so_in,celldex::MonacoImmuneData(),"label.fine")
-    so_in$immu_cell_exp_main <-  runSingleR(so_in,celldex::DatabaseImmuneCellExpressionData(),
-                                            "label.main")
-    so_in$immu_cell_exp <- runSingleR(so_in,celldex::DatabaseImmuneCellExpressionData(),
-                                      "label.fine")
-    so_in$annot = so_in$HPCA_main
-  } else if(ref == "mm10"){
-    so_in$immgen_main <-  runSingleR(so_in,celldex::ImmGenData(),"label.main")
-    so_in$immgen <- runSingleR(so_in,celldex::ImmGenData(),"label.fine")
-    so_in$mouseRNAseq_main <-  runSingleR(so_in,celldex::MouseRNAseqData(),"label.main")
-    so_in$mouseRNAseq <- runSingleR(so_in,celldex::MouseRNAseqData(),"label.fine")
-    so_in$annot = so_in$immgen_main
+  # run PCA, FindNeighbors
+  obj <- RunPCA(object = obj, npcs = npcs, verbose = FALSE)
+  obj <- FindNeighbors(obj,dims = 1:npcs)
+  
+  # for each input resolution, find clusters
+  for (res in resolution){
+    obj <- FindClusters(obj,dims = 1:npcs, 
+                        print.output = 0, resolution = res,algorithm = 3)
   }
-  return(so_in)
+  
+  # rename cols
+  colnames(obj@meta.data) = gsub("integrated_snn_res","SLM_int_snn_res",colnames(obj@meta.data))
+  colnames(obj@meta.data) = gsub("SCT_snn_res","SLM_SCT_snn_res",colnames(obj@meta.data))
+  
+  # run UMAP
+  obj <- RunUMAP(object = obj, reduction = "pca",
+                 dims = 1:npcs,n.components = 3)
+  
+  # this is essentially the same labeling as was done in preprocessing, except now you're adding
+  # "clustAnnot" in the front - is it ncessary? can we keep the obj$HPCA_main, or is the prefix needed
+  # for all of these?
+  if(ref == "hg38" || ref == "hg19"){
+    obj$clustAnnot_HPCA_main <- RUN_CORRECTION_SEURAT(obj,celldex::HumanPrimaryCellAtlasData(),"label.main")
+    obj$clustAnnot_HPCA <-  RUN_CORRECTION_SEURAT(obj,celldex::HumanPrimaryCellAtlasData(),"label.fine")
+    obj$clustAnnot_BP_encode_main <-  RUN_CORRECTION_SEURAT(obj,celldex::BlueprintEncodeData(),"label.main")
+    obj$clustAnnot_BP_encode <-  RUN_CORRECTION_SEURAT(obj,celldex::BlueprintEncodeData(),"label.fine")
+    obj$clustAnnot_monaco_main <-  RUN_CORRECTION_SEURAT(obj,celldex::MonacoImmuneData(),"label.main")
+    obj$clustAnnot_monaco <- RUN_CORRECTION_SEURAT(obj,celldex::MonacoImmuneData(),"label.fine")
+    obj$clustAnnot_immu_cell_exp_main <-  RUN_CORRECTION_SEURAT(obj,celldex::DatabaseImmuneCellExpressionData(),"label.main")
+    obj$clustAnnot_immu_cell_exp <- RUN_CORRECTION_SEURAT(obj,celldex::DatabaseImmuneCellExpressionData(),"label.fine")
+  } else if(ref == "mm10"){
+    obj$clustAnnot_immgen_main <-  RUN_CORRECTION_SEURAT(obj,celldex::ImmGenData(),"label.main")
+    obj$clustAnnot_immgen <- RUN_CORRECTION_SEURAT(obj,celldex::ImmGenData(),"label.fine")
+    obj$clustAnnot_mouseRNAseq_main <-  RUN_CORRECTION_SEURAT(obj,celldex::MouseRNAseqData(),"label.main")
+    obj$clustAnnot_mouseRNAseq <- RUN_CORRECTION_SEURAT(obj,celldex::MouseRNAseqData(),"label.fine")
+  }
+  return(obj)
 }
 
-runSingleR = function(obj,refFile,fineORmain){
+##################################################################
+#
+##################################################################
+RUN_SINGLEr_AVERAGE = function(obj,refFile,fineORmain){
   avg = AverageExpression(obj,assays = "SCT")
   avg = as.data.frame(avg)
   ref = refFile
@@ -129,36 +204,90 @@ runSingleR = function(obj,refFile,fineORmain){
   return(obj$clustAnnot)
 }
 
-runInt = function(obj,npcs){
-  if(ncol(obj@assays$integrated@scale.data)!=ncol(obj@assays$integrated@data)){
-    obj = ScaleData(obj)
-  }
-  obj <- RunPCA(object = obj, npcs = npcs, verbose = FALSE)
-  obj <- FindNeighbors(obj,dims = 1:npcs)
-  
+FIND_CLUSTERS_BY_RES<-function(resolution,obj,algorithm_val){
   for (res in resolution){
-    obj <- FindClusters(obj,dims = 1:npcs, print.output = 0, resolution = res,algorithm = 3)#
+    obj <- FindClusters(obj,dims = 1:npcs, 
+                        print.output = 0, resolution = res,algorithm = algorithm_val)
   }
+  return(obj)
+}
+
+RUN_CORRECTION_HARMONY = function(obj,npcs,harm, resolution){
+  obj <- RunPCA(object = obj, npcs = npcs, verbose = FALSE)
+  
+  if(harm=="Yes") {
+    obj <- FindNeighbors(obj,dims = 1:npcs,reduction = "harmony")
+  } else if(harm=="No") {
+    obj <- FindNeighbors(obj,dims = 1:npcs)
+  }
+  
+  # for each input resolution, find clusters
+  FIND_CLUSTERS_BY_RES(obj,res,3)
   colnames(obj@meta.data) = gsub("integrated_snn_res","SLM_int_snn_res",colnames(obj@meta.data))
   colnames(obj@meta.data) = gsub("SCT_snn_res","SLM_SCT_snn_res",colnames(obj@meta.data))
   
-  obj <- RunUMAP(object = obj, reduction = "pca",
-                 dims = 1:npcs,n.components = 3)
+  FIND_CLUSTERS_BY_RES(obj,res,4)
+  colnames(obj@meta.data) = gsub("integrated_snn_res","Leiden_int_snn_res",colnames(obj@meta.data))
+  colnames(obj@meta.data) = gsub("SCT_snn_res","Leiden_SCT_snn_res",colnames(obj@meta.data))
   
+  if(harm=="Yes") {
+    obj <- RunUMAP(object = obj,dims = 1:npcs, n.components = 3,reduction = "harmony")
+  } else if(harm=="No") {
+    obj <- RunUMAP(object = obj, reduction = "pca",dims = 1:npcs,n.components = 3)
+  }
+
+  # this is essentially the same labeling as was done in preprocessing, except now you're adding
+  # "clustAnnot" in the front - is it ncessary? can we keep the obj$HPCA_main, or is the prefix needed
+  # for all of these?
   if(ref == "hg38" || ref == "hg19"){
-    obj$clustAnnot_HPCA_main <- runSingleR(obj,celldex::HumanPrimaryCellAtlasData(),"label.main")
-    obj$clustAnnot_HPCA <-  runSingleR(obj,celldex::HumanPrimaryCellAtlasData(),"label.fine")
-    obj$clustAnnot_BP_encode_main <-  runSingleR(obj,celldex::BlueprintEncodeData(),"label.main")
-    obj$clustAnnot_BP_encode <-  runSingleR(obj,celldex::BlueprintEncodeData(),"label.fine")
-    obj$clustAnnot_monaco_main <-  runSingleR(obj,celldex::MonacoImmuneData(),"label.main")
-    obj$clustAnnot_monaco <- runSingleR(obj,celldex::MonacoImmuneData(),"label.fine")
-    obj$clustAnnot_immu_cell_exp_main <-  runSingleR(obj,celldex::DatabaseImmuneCellExpressionData(),"label.main")
-    obj$clustAnnot_immu_cell_exp <- runSingleR(obj,celldex::DatabaseImmuneCellExpressionData(),"label.fine")
+    obj$clustAnnot_HPCA_main <- RUN_SINGLEr_AVERAGE(obj,celldex::HumanPrimaryCellAtlasData(),"label.main")
+    obj$clustAnnot_HPCA <-  RUN_SINGLEr_AVERAGE(obj,celldex::HumanPrimaryCellAtlasData(),"label.fine")
+    obj$clustAnnot_BP_encode_main <-  RUN_SINGLEr_AVERAGE(obj,celldex::BlueprintEncodeData(),"label.main")
+    obj$clustAnnot_BP_encode <-  RUN_SINGLEr_AVERAGE(obj,celldex::BlueprintEncodeData(),"label.fine")
+    obj$clustAnnot_monaco_main <-  RUN_SINGLEr_AVERAGE(obj,celldex::MonacoImmuneData(),"label.main")
+    obj$clustAnnot_monaco <- RUN_SINGLEr_AVERAGE(obj,celldex::MonacoImmuneData(),"label.fine")
+    obj$clustAnnot_immu_cell_exp_main <-  RUN_SINGLEr_AVERAGE(obj,celldex::DatabaseImmuneCellExpressionData(),"label.main")
+    obj$clustAnnot_immu_cell_exp <- RUN_SINGLEr_AVERAGE(obj,celldex::DatabaseImmuneCellExpressionData(),"label.fine")
+  }
+  
+  if(ref == "mm10"){
+    obj$clustAnnot_immgen_main <-  RUN_SINGLEr_AVERAGE(obj,celldex::ImmGenData(),"label.main")
+    obj$clustAnnot_immgen <- RUN_SINGLEr_AVERAGE(obj,celldex::ImmGenData(),"label.fine")
+    obj$clustAnnot_mouseRNAseq_main <-  RUN_SINGLEr_AVERAGE(obj,celldex::MouseRNAseqData(),"label.main")
+    obj$clustAnnot_mouseRNAseq <- RUN_SINGLEr_AVERAGE(obj,celldex::MouseRNAseqData(),"label.fine")
+  }
+  return(obj)
+}
+
+RUN_CORRECTION_RPCA = function(obj,npcs){
+  obj <- RunPCA(object = obj, npcs = npcs, verbose = FALSE)
+  obj <- FindNeighbors(obj,dims = 1:npcs)
+  
+  FIND_CLUSTERS_BY_RES(obj,res,3)
+  colnames(obj@meta.data) = gsub("integrated_snn_res","SLM_int_snn_res",colnames(obj@meta.data))
+  colnames(obj@meta.data) = gsub("SCT_snn_res","SLM_SCT_snn_res",colnames(obj@meta.data))
+  
+  FIND_CLUSTERS_BY_RES(obj,res,4)
+  colnames(obj@meta.data) = gsub("integrated_snn_res","Leiden_int_snn_res",colnames(obj@meta.data))
+  colnames(obj@meta.data) = gsub("SCT_snn_res","Leiden_SCT_snn_res",colnames(obj@meta.data))
+  
+  obj <- RunUMAP(object = obj, reduction = "pca", 
+                 dims = 1:npcs,n.components = 3)
+
+  if(ref == "hg38" || ref == "hg19"){
+    obj$clustAnnot_HPCA_main <- RUN_SINGLEr_AVERAGE(obj,celldex::HumanPrimaryCellAtlasData(),"label.main")
+    obj$clustAnnot_HPCA <-  RUN_SINGLEr_AVERAGE(obj,celldex::HumanPrimaryCellAtlasData(),"label.fine")
+    obj$clustAnnot_BP_encode_main <-  RUN_SINGLEr_AVERAGE(obj,celldex::BlueprintEncodeData(),"label.main")
+    obj$clustAnnot_BP_encode <-  RUN_SINGLEr_AVERAGE(obj,celldex::BlueprintEncodeData(),"label.fine")
+    obj$clustAnnot_monaco_main <-  RUN_SINGLEr_AVERAGE(obj,celldex::MonacoImmuneData(),"label.main")
+    obj$clustAnnot_monaco <- RUN_SINGLEr_AVERAGE(obj,celldex::MonacoImmuneData(),"label.fine")
+    obj$clustAnnot_immu_cell_exp_main <-  RUN_SINGLEr_AVERAGE(obj,celldex::DatabaseImmuneCellExpressionData(),"label.main")
+    obj$clustAnnot_immu_cell_exp <- RUN_SINGLEr_AVERAGE(obj,celldex::DatabaseImmuneCellExpressionData(),"label.fine")
   } else if(ref == "mm10"){
-    obj$clustAnnot_immgen_main <-  runSingleR(obj,celldex::ImmGenData(),"label.main")
-    obj$clustAnnot_immgen <- runSingleR(obj,celldex::ImmGenData(),"label.fine")
-    obj$clustAnnot_mouseRNAseq_main <-  runSingleR(obj,celldex::MouseRNAseqData(),"label.main")
-    obj$clustAnnot_mouseRNAseq <- runSingleR(obj,celldex::MouseRNAseqData(),"label.fine")
+    obj$clustAnnot_immgen_main <-  RUN_SINGLEr_AVERAGE(obj,celldex::ImmGenData(),"label.main")
+    obj$clustAnnot_immgen <- RUN_SINGLEr_AVERAGE(obj,celldex::ImmGenData(),"label.fine")
+    obj$clustAnnot_mouseRNAseq_main <-  RUN_SINGLEr_AVERAGE(obj,celldex::MouseRNAseqData(),"label.main")
+    obj$clustAnnot_mouseRNAseq <- RUN_SINGLEr_AVERAGE(obj,celldex::MouseRNAseqData(),"label.fine")
   }
   return(obj)
 }
