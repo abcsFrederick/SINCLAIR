d1f6349af0780280a6e79148937ceba075c38a8a
slsevilla <slsevilla@gmail.com>
2023-06-06 10:28:22 -0400

    cleanup functions for all RMDs

--- bin/scRNA_functions.R
+++ bin/scRNA_functions.R
@@ -57,7 +57,7 @@ RUN_SINGLEr = function(obj,refFile,fineORmain){
   return(s$pruned.labels)
 }
 
-MAIN_LABEL_SO<-function(so_in, species){
+MAIN_SINGLER<-function(so_in, species){
   if(species == "hg38" || species == "hg19"){
     so_in$HPCA_main <- RUN_SINGLEr(so_in,celldex::HumanPrimaryCellAtlasData(),"label.main")
     so_in$HPCA <-  RUN_SINGLEr(so_in,celldex::HumanPrimaryCellAtlasData(),"label.fine")
@@ -83,7 +83,7 @@ MAIN_LABEL_SO<-function(so_in, species){
 ##################################################################
 #
 ##################################################################
-PROCESS_DOUBLETS <-function(so_in, run_doublet_finder){
+MAIN_DOUBLETS <-function(so_in, run_doublet_finder){
   if (run_doublet_finder=="Y"){
     sweep.res.list_kidney <- paramSweep_v3(so_in,PCs = 1:10, sct = T)
     sweep.stats_kidney <- summarizeSweep(sweep.res.list_kidney, GT = FALSE)
@@ -114,70 +114,7 @@ PROCESS_DOUBLETS <-function(so_in, run_doublet_finder){
 }
 
 ##################################################################
-#
-##################################################################
-RUN_SINGLEr_seurat = function(obj,refFile,fineORmain){
-  # find av expressions
-  avg = AverageExpression(obj,assays = "SCT")
-  avg = as.data.frame(avg)
-  
-  s = SingleR(test = as.matrix(avg),ref = refFile,labels = refFile[[fineORmain]])
-  
-  clustAnnot = s$labels
-  names(clustAnnot) = colnames(avg)
-  names(clustAnnot) = gsub("SCT.","",names(clustAnnot))
-  
-  obj$clustAnnot = clustAnnot[match(obj$seurat_clusters,names(clustAnnot))]
-  return(obj$clustAnnot)
-}
-
-RUN_CORRECTION_SEURAT = function(obj,npcs,species,resolution){
-  # what is this catching? why another scale?
-  if(ncol(obj@assays$integrated@scale.data)!=ncol(obj@assays$integrated@data)){
-    obj = ScaleData(obj)
-  }
-  
-  # run PCA, FindNeighbors
-  obj <- RunPCA(object = obj, npcs = npcs, verbose = FALSE)
-  obj <- FindNeighbors(obj,dims = 1:npcs)
-  
-  # for each input resolution, find clusters
-  for (res in resolution){
-    obj <- FindClusters(obj,dims = 1:npcs, 
-                        print.output = 0, resolution = res,algorithm = 3)
-  }
-  
-  # rename cols
-  colnames(obj@meta.data) = gsub("integrated_snn_res","SLM_int_snn_res",colnames(obj@meta.data))
-  colnames(obj@meta.data) = gsub("SCT_snn_res","SLM_SCT_snn_res",colnames(obj@meta.data))
-  
-  # run UMAP
-  obj <- RunUMAP(object = obj, reduction = "pca",
-                 dims = 1:npcs,n.components = 3)
-  
-  # this is essentially the same labeling as was done in preprocessing, except now you're adding
-  # "clustAnnot" in the front - is it ncessary? can we keep the obj$HPCA_main, or is the pspeciesix needed
-  # for all of these?
-  if(species == "hg38" || species == "hg19"){
-    obj$clustAnnot_HPCA_main <- RUN_CORRECTION_SEURAT(obj,celldex::HumanPrimaryCellAtlasData(),"label.main")
-    obj$clustAnnot_HPCA <-  RUN_CORRECTION_SEURAT(obj,celldex::HumanPrimaryCellAtlasData(),"label.fine")
-    obj$clustAnnot_BP_encode_main <-  RUN_CORRECTION_SEURAT(obj,celldex::BlueprintEncodeData(),"label.main")
-    obj$clustAnnot_BP_encode <-  RUN_CORRECTION_SEURAT(obj,celldex::BlueprintEncodeData(),"label.fine")
-    obj$clustAnnot_monaco_main <-  RUN_CORRECTION_SEURAT(obj,celldex::MonacoImmuneData(),"label.main")
-    obj$clustAnnot_monaco <- RUN_CORRECTION_SEURAT(obj,celldex::MonacoImmuneData(),"label.fine")
-    obj$clustAnnot_immu_cell_exp_main <-  RUN_CORRECTION_SEURAT(obj,celldex::DatabaseImmuneCellExpressionData(),"label.main")
-    obj$clustAnnot_immu_cell_exp <- RUN_CORRECTION_SEURAT(obj,celldex::DatabaseImmuneCellExpressionData(),"label.fine")
-  } else if(species == "mm10"){
-    obj$clustAnnot_immgen_main <-  RUN_CORRECTION_SEURAT(obj,celldex::ImmGenData(),"label.main")
-    obj$clustAnnot_immgen <- RUN_CORRECTION_SEURAT(obj,celldex::ImmGenData(),"label.fine")
-    obj$clustAnnot_mouseRNAseq_main <-  RUN_CORRECTION_SEURAT(obj,celldex::MouseRNAseqData(),"label.main")
-    obj$clustAnnot_mouseRNAseq <- RUN_CORRECTION_SEURAT(obj,celldex::MouseRNAseqData(),"label.fine")
-  }
-  return(obj)
-}
-
-##################################################################
-#
+# run batch corrections
 ##################################################################
 RUN_SINGLEr_AVERAGE = function(obj,refFile,fineORmain){
   avg = AverageExpression(obj,assays = "SCT")
@@ -194,88 +131,19 @@ RUN_SINGLEr_AVERAGE = function(obj,refFile,fineORmain){
   return(obj$clustAnnot)
 }
 
-FIND_CLUSTERS_BY_RES<-function(resolution,obj,algorithm_val){
-  for (res in resolution){
-    obj <- FindClusters(obj,dims = 1:npcs, 
-                        print.output = 0, resolution = res,algorithm = algorithm_val)
-  }
-  return(obj)
-}
-
-RUN_CORRECTION_HARMONY = function(so_in,npcs,species,resolution_list){
-  # transform and runPCA
-  so_transform <- SCTransform(so_in)
-  so_pca <- RunPCA(so_transform)
-  
-  # integrate
-  so_integrate <- IntegrateLayers(object = so_pca, method = HarmonyIntegration, normalization.method = "SCT", verbose = F,new.reduction = "harmony")
-  so <- RunUMAP(so_integrate, reduction = "RPCA", dims = 1:npcs)
-  so <- FindNeighbors(so, reduction = "RPCA", dims = 1:npcs)
-
-  for (res in resolution_list){
-    so <- FindClusters(so, dims = 1:npcs, resolution = res, algorithm = 3)
-  }
-
-  if(species == "hg38" || species == "hg19"){
-    so$clustAnnot_HPCA_main <- RUN_SINGLEr_AVERAGE(so,celldex::HumanPrimaryCellAtlasData(),"label.main")
-    so$clustAnnot_HPCA <-  RUN_SINGLEr_AVERAGE(so,celldex::HumanPrimaryCellAtlasData(),"label.fine")
-    so$clustAnnot_BP_encode_main <-  RUN_SINGLEr_AVERAGE(so,celldex::BlueprintEncodeData(),"label.main")
-    so$clustAnnot_BP_encode <-  RUN_SINGLEr_AVERAGE(so,celldex::BlueprintEncodeData(),"label.fine")
-    so$clustAnnot_monaco_main <-  RUN_SINGLEr_AVERAGE(so,celldex::MonacoImmuneData(),"label.main")
-    so$clustAnnot_monaco <- RUN_SINGLEr_AVERAGE(so,celldex::MonacoImmuneData(),"label.fine")
-    so$clustAnnot_immu_cell_exp_main <-  RUN_SINGLEr_AVERAGE(so,celldex::DatabaseImmuneCellExpressionData(),"label.main")
-    so$clustAnnot_immu_cell_exp <- RUN_SINGLEr_AVERAGE(so,celldex::DatabaseImmuneCellExpressionData(),"label.fine")
-  }
-  
-  if(species == "mm10"){
-    so$clustAnnot_immgen_main <-  RUN_SINGLEr_AVERAGE(so,celldex::ImmGenData(),"label.main")
-    so$clustAnnot_immgen <- RUN_SINGLEr_AVERAGE(so,celldex::ImmGenData(),"label.fine")
-    so$clustAnnot_mouseRNAseq_main <-  RUN_SINGLEr_AVERAGE(so,celldex::MouseRNAseqData(),"label.main")
-    so$clustAnnot_mouseRNAseq <- RUN_SINGLEr_AVERAGE(so,celldex::MouseRNAseqData(),"label.fine")
-  }
-
-  # obj <- RunPCA(object = obj, npcs = npcs, verbose = FALSE)
-  
-  # if(harm=="Yes") {
-  #   obj <- FindNeighbors(obj,dims = 1:npcs,reduction = "harmony")
-  # } else if(harm=="No") {
-  #   obj <- FindNeighbors(obj,dims = 1:npcs)
-  # }
-  
-  # # for each input resolution, find clusters
-  # FIND_CLUSTERS_BY_RES(obj,res,3)
-  # colnames(obj@meta.data) = gsub("integrated_snn_res","SLM_int_snn_res",colnames(obj@meta.data))
-  # colnames(obj@meta.data) = gsub("SCT_snn_res","SLM_SCT_snn_res",colnames(obj@meta.data))
-  
-  # FIND_CLUSTERS_BY_RES(obj,res,4)
-  # colnames(obj@meta.data) = gsub("integrated_snn_res","Leiden_int_snn_res",colnames(obj@meta.data))
-  # colnames(obj@meta.data) = gsub("SCT_snn_res","Leiden_SCT_snn_res",colnames(obj@meta.data))
-  
-  # if(harm=="Yes") {
-  #   obj <- RunUMAP(object = obj,dims = 1:npcs, n.components = 3,reduction = "harmony")
-  # } else if(harm=="No") {
-  #   obj <- RunUMAP(object = obj, reduction = "pca",dims = 1:npcs,n.components = 3)
-  # }
-
-  # this is essentially the same labeling as was done in preprocessing, except now you're adding
-  # "clustAnnot" in the front - is it ncessary? can we keep the obj$HPCA_main, or is the prefix needed
-  # for all of these?
-
-  return(so)
-}
-
-RUN_CORRECTION_RPCA = function(so_in,npcs,species,resolution_list){
+MAIN_BATCH_CORRECTION = function(so_in,npcs,species,resolution_list,method_in,reduction_in){
    # transform and runPCA
   so_transform <- SCTransform(so_in)
   so_pca <- RunPCA(so_transform)
   
   # integrate
-  so_integrate <- IntegrateLayers(object = so_pca, method = RPCAIntegration, normalization.method = "SCT", verbose = F,new.reduction = "RPCA")
+  so_integrate <- IntegrateLayers(object = so_pca, method = get(method_in), normalization.method = "SCT", verbose = F,new.reduction = reduction_in)
   so <- RunPCA(object = so_integrate, npcs = npcs, verbose = FALSE)
-  so <- FindNeighbors(so,dims = 1:npcs)
+  so <- RunUMAP(so, reduction = reduction_in, dims = 1:npcs)
+  so <- FindNeighbors(so, reduction = reduction_in, dims = 1:npcs)
   
   for (res in resolution_list){
-    so <- FindClusters(so,dims = 1:npcs, resolution = res,algorithm = 3)
+    so <- FindClusters(so,dims = 1:npcs, resolution = res, algorithm = 3)
   }
 
   # relabel
@@ -295,4 +163,30 @@ RUN_CORRECTION_RPCA = function(so_in,npcs,species,resolution_list){
     so$clustAnnot_mouseRNAseq <- RUN_SINGLEr_AVERAGE(so,celldex::MouseRNAseqData(),"label.fine")
   }
   return(so)
+}
+
+##################################################################
+#
+##################################################################
+OBJECT_SELECT = function(id){
+  obj = switch(
+    id,
+    "merged"=so_merged,
+    "integrated"=so_integrated,
+    "rpca"=so_rpca,
+    "harmonyGroup"=so_harmonyGroup,
+    "harmonySample"=so_harmonySample
+  )
+  return (obj)
+}
+NAME_SELECT = function(id){
+  obj = switch(
+    id,
+    "merged"="Merged",
+    "integrated"="Integrated CCA",
+    "rpca"="RPCA",
+    "harmonyGroup"="Harmony (Group)",
+    "harmonySample"="Harmony (Sample)"
+  )
+  return (obj)
 }
