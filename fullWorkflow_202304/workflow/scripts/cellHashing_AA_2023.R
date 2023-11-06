41f0ee575c3617d658f3f48a5182b22e8d7d9a84
Nathan W <38703762+wong-nw@users.noreply.github.com>
2023-11-06 13:55:07 -0500

    File upload (20231106_NW)
    Files include code snippets for including V(D)J data, CITESeq data with normalization by CLR and DSB techniques, and Cell Hashing/Demultiplexing

---
+++ fullWorkflow_202304/workflow/scripts/cellHashing_AA_2023.R
@@ -0,0 +1,124 @@
+loc = "/data/CCBR_Pipeliner/db/PipeDB/scrna4.2Rlibs"
+library(Seurat,lib.loc=loc)
+library(stringr)#,lib.loc=loc)
+library("DoubletFinder",lib.loc=loc)
+library(SingleR,lib.loc=loc)
+library(scRNAseq,lib.loc=loc)
+library(SingleCellExperiment,lib.loc=loc)
+library(celldex,lib.loc=loc)
+library(Orthology.eg.db,lib.loc=loc)
+library(org.Mm.eg.db,lib.loc=loc)
+library(org.Hs.eg.db,lib.loc=loc)
+library(flexmix,lib.loc=loc)
+library(SeuratWrappers,lib.loc=loc)
+
+
+ref = "mm10"
+
+hashingTable = read.delim("hashing.tab",header=F,stringsAsFactors = F)
+colnames(hashingTable) = c("Sample","HTO","name")
+
+h5 = Read10X_h5("filtered_feature_bc_matrix.h5")
+
+so = CreateSeuratObject(h5$'Gene Expression')
+
+so = NormalizeData(so)
+
+so = FindVariableFeatures(so, selection.method = "mean.var.plot")
+
+so = ScaleData(so, features = VariableFeatures(so))
+
+
+cite = h5$'Antibody Capture'
+cite = cite[grep("HTO",rownames(cite)),] 
+rownames(cite) = hashingTable$name[match(rownames(cite),hashingTable$HTO,nomatch=F)]
+print(rownames(cite))
+
+so[["HTO"]] <- CreateAssayObject(counts = cite) 
+
+
+
+so = NormalizeData(so, assay = "HTO", normalization.method = "CLR")
+
+so <- HTODemux(so, assay = "HTO", positive.quantile = 0.99)
+
+Idents(so) = "HTO_classification.global"
+
+so = subset(so, idents = "Singlet")
+
+
+runSingleR = function(obj,refFile,fineORmain){
+sce = as.SingleCellExperiment(obj,assay = "RNA")
+ref = refFile
+s = SingleR(test = sce, ref = ref,labels = ref[[fineORmain]])
+return(s$pruned.labels)
+print(head(s$pruned.labels))
+}
+
+
+if(ref == "hg38"){
+so$HPCA_main <- runSingleR(so,celldex::HumanPrimaryCellAtlasData(),"label.main")
+so$HPCA <-  runSingleR(so,celldex::HumanPrimaryCellAtlasData(),"label.fine")
+so$BP_encode_main <-  runSingleR(so,celldex::BlueprintEncodeData(),"label.main")
+so$BP_encode <-  runSingleR(so,celldex::BlueprintEncodeData(),"label.fine")
+so$monaco_main <-  runSingleR(so,celldex::MonacoImmuneData(),"label.main")
+so$monaco <-     runSingleR(so,celldex::MonacoImmuneData(),"label.fine")
+so$immu_cell_exp_main <-  runSingleR(so,celldex::DatabaseImmuneCellExpressionData(),"label.main")
+so$immu_cell_exp <- runSingleR(so,celldex::DatabaseImmuneCellExpressionData(),"label.fine")
+so$annot = so$HPCA_main
+}
+
+if(ref == "mm10"){
+
+so$immgen_main <-  runSingleR(so,celldex::ImmGenData(),"label.main")
+so$immgen <- runSingleR(so,celldex::ImmGenData(),"label.fine")
+so$mouseRNAseq_main <-  runSingleR(so,celldex::MouseRNAseqData(),"label.main")
+so$mouseRNAseq <- runSingleR(so,celldex::MouseRNAseqData(),"label.fine")
+
+so$annot = so$immgen_main
+
+}
+
+
+
+DF = as.data.frame(as.matrix(so@assays$HTO@data))
+so$assignHTO = rownames(DF)[apply(DF,2,which.max)]
+so.list = SplitObject(so,split.by="assignHTO")
+
+#remove samples with less than 100 cells 
+ncells = vector()
+for(n in 1:length(so.list) ){ncells = append(ncells,ncol(so.list[[n]])) }
+
+so.list = so.list[ncells > 100]
+
+
+
+convertHumanGeneList  <- function(gns){
+  egs <- mapIds(org.Hs.eg.db, gns, "ENTREZID","SYMBOL")
+  mapped <- select(Orthology.eg.db, egs, "Mus.musculus","Homo.sapiens")
+  mapped$MUS <- mapIds(org.Mm.eg.db, as.character(mapped$Mus.musculus), "SYMBOL", "ENTREZID")
+  return(as.character(unlist(mapped$MUS )))
+}
+
+
+
+#if(ref=="hg38"){so[["percent.mt"]] <- PercentageFeatureSet(so, pattern = "^MT-")}
+#if(ref=="mm10"){so[["percent.mt"]] <- PercentageFeatureSet(so, pattern = "^mt-")}
+
+
+
+print(names(so.list))
+
+so.list <- lapply(X = so.list, FUN = NormalizeData,normalization.method = "LogNormalize", scale.factor = 10000,assay = "RNA")
+so.list <- lapply(X = so.list, FUN = SCTransform,variable.features.rv.th = 1.3,variable.features.n = NULL)
+so.list <- lapply(X = so.list, FUN = RunPCA,npcs = 30)
+so.list <- lapply(X = so.list, FUN = RunUMAP, dims = 1:30)
+so.list <- lapply(X = so.list, FUN = FindNeighbors, dims = 1:30)
+so.list <- lapply(X = so.list, FUN = FindClusters, algorithm = 3)
+so.list <- lapply(X = so.list, FUN = saveRDS, paste0("x"))
+
+for(n in 1:length(so.list)){}
+print(names(so.list))
+
+for(n in 1:length(so.list)){saveRDS(so.list[[n]] , paste0("x_",names(so.list)[n]),".rds")}
+
